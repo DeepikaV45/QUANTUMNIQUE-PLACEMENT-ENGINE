a2aa53031071c5ac64fe54dda4f6ce0e66892c43
Deepika V <v.deepika.u@gmail.com>
2026-03-17 14:17:38 +0530

    Add files via upload

---
+++ Day2/MaximumSubarray.java
@@ -0,0 +1,32 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class MaximumSubarray {
+
+    static int MaxSubArray(int[] arr){
+        int sum=arr[0];
+        int maxsum=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(sum>=0){
+                sum=sum+arr[i];
+            }else{
+                sum=arr[i];
+            }
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+        }
+        return maxsum;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(MaxSubArray(arr));
+    }
+}
