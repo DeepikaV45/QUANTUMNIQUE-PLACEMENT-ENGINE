ea953fb45fee0b3f5b9cabe58b37ad0759995402
Deepika V <v.deepika.u@gmail.com>
2026-03-23 17:37:04 +0530

    Update SquareRootOfNumber.java

--- Day5/SquareRootOfNumber.java
+++ Day5/SquareRootOfNumber.java
@@ -2,29 +2,27 @@ package Day5;
 import java.util.Scanner;
 public class SquareRootOfNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n < 0) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<0){
             System.out.println("Invalid input");
             return;
         }
-        if (n == 0 || n == 1) {
+        if(n==0||n==1){
             System.out.println(n);
             return;
         }
-         long low = 1, high = n, ans = 0;
-        
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
-            
-            if (mid * mid == n) {
-                ans = mid;
+         long low=1, high=n, ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(mid*mid==n){
+                ans=mid;
                 break;
-            } else if (mid * mid < n) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            }else if(mid*mid<n){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
         
